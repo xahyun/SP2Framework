53efb951bae40d17a5ce90003d6ee03352328576
xahyun <88998867+xahyun@users.noreply.github.com>
2022-02-16 00:52:25 +0800

    To be able to change scene
    changes were made in application CPP in order to switch between scenes

--- Application/Source/Application.cpp
+++ Application/Source/Application.cpp
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 
 #include "SceneTemplate.h"
+#include "SceneOffice.h"
 
 GLFWwindow* m_window;
 const unsigned char FPS = 60; // FPS of this game
@@ -150,24 +151,32 @@ void Application::Init()
 void Application::Run()
 {
 	//Main Loop
-	Scene* scene = new SceneTemplate();
-	scene->Init();
+	Scene* scene1 = new SceneTemplate();
+	Scene* scene2 = new SceneOffice();
+	Scene* scene = scene1;
+	scene1->Init();
+	scene2->Init();
 
 	m_timer.startTimer();    // Start timer to calculate how long it takes to render this frame
 	while (!glfwWindowShouldClose(m_window) && !IsKeyPressed(VK_ESCAPE))
 	{
 		scene->Update(m_timer.getElapsedTime());
 		scene->Render();
+		if (IsKeyPressed(VK_F1))
+			scene = scene1;
+		else if (IsKeyPressed(VK_F2))
+			scene = scene2;
 		//Swap buffers
 		glfwSwapBuffers(m_window);
 		//Get and organize events, like keyboard and mouse input, window resizing, etc...
 		glfwPollEvents();
-        m_timer.waitUntil(frameTime);       // Frame rate limiter. Limits each frame to a specified time in ms.
+		m_timer.waitUntil(frameTime);    // Frame rate limiter. Limits each frame to a specified time in ms.   
 
 	} //Check if the ESC key had been pressed or if the window had been closed
-
-	scene->Exit();
-	delete scene;
+	scene1->Exit();
+	scene2->Exit();
+	delete scene1;
+	delete scene2;
 }
 
 void Application::Exit()
