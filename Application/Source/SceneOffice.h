1e5727297b64d5e97dff4f10da0d6c973e921cfd
Timothy Luk <81371980+timothotorsmt@users.noreply.github.com>
2022-02-21 10:54:44 +0800

    edit sceneOffice.h
    periodttttt

--- Application/Source/SceneOffice.h
+++ Application/Source/SceneOffice.h
@@ -10,11 +10,26 @@
 #include "Light.h"
 #include "LoadTGA.h"
 #include "Material.h"
+#include "clock.h"
+#include "player.h"
+#include <utility>
+#include <string>
+#include <vector>
+#include <tuple>
 
 #define SKYBOXSIZE 1000
 #define PLAYERSPEED 2.0f
 
-class SceneOffice: public Scene
+struct products {
+	bool hasPicture;
+	bool isLimitedEdition;
+	int reviewStars;
+	std::string productName;
+	int productPrice;
+	bool isScam;
+};
+
+class SceneOffice : public Scene
 {
 	enum GEOMETRY_TYPE {
 		GEO_AXES,
@@ -33,6 +48,46 @@ class SceneOffice: public Scene
 		GEO_TOP,
 		GEO_BOTTOM,
 
+		//otherUI
+		GEO_WATCH,
+		GEO_HAPPY_FACE,
+		GEO_ANGRY_FACE,
+
+		//phone ui
+		GEO_NOTIF_BOX,
+		GEO_PHONE_UI,
+		GEO_CHAT_ICON,
+		GEO_PHONE_ICON,
+		GEO_HOME_ICON,
+		GEO_MONEY_ICON,
+		GEO_SOCIALSCORE_ICON,
+		GEO_SOCIALSCORE_UI,
+		GEO_SOCIALSCORE_BAR,
+		GEO_TASKS_ICON,
+		GEO_HELP_ICON,
+		GEO_SETTINGS_ICON,
+
+		//computer UI,
+		GEO_DEFAULT_WALLPAPER,
+		GEO_POWER_BUTTON,
+		GEO_EMAIL_ICON,
+		GEO_SHOPPING_ICON,
+		GEO_WORK_ICON,
+		GEO_USER_ICON,
+		GEO_BUTTON_PRESSED,
+		GEO_BUTTON_NOT_PRESSED,
+
+		//shopping thing UI
+		GEO_SHOP_LOGO,
+		GEO_SHOP_MENU,
+		GEO_ITEM_NOIMAGE,
+		GEO_ITEM_PHONE,
+		GEO_ITEM_WATCH,
+		GEO_ITEM_CABLE,
+		GEO_STAR,
+		GEO_LIMITED_EDITION,
+
+		//office items
 		GEO_OFFICETABLE,
 		GEO_COMPUTER,
 		GEO_KEYBOARD,
@@ -89,14 +144,47 @@ class SceneOffice: public Scene
 		U_TOTAL,
 	};
 
+	enum PhoneScreen {
+		P_PHONEOFF,
+		P_HOME,
+		P_CHAT,
+		P_CHATLIST,
+		P_MONEY,
+		P_SOCIALSCORE,
+		P_HELP,
+		P_TASKS
+	};
+
+	enum computerScreen {
+		C_COMOFF,
+		C_LOADING,
+		C_HOME,
+		C_EMAIL,
+		C_EMAIL_EXPLAINING,
+		C_SHOPPINGPAGE_MENU,
+		C_SHOPPINGPAGE,
+		C_CLOCKIN,
+		C_TASKS,
+		C_WORK
+	};
+
+	enum Notification {
+		N_OFF,
+		N_CHAT,
+		N_TASK,
+		N_TASK_DONE,
+		N_WORK
+	};
+
 public:
 	SceneOffice();
 	~SceneOffice();
 
-	virtual void Init();
-	virtual void Update(double dt);
-	virtual void Render();
-	virtual void Exit();
+	void Init();
+	void TransferGameInfo(Game* game);
+	void Update(double dt);
+	void Render();
+	void Exit();
 
 private:
 	unsigned m_vertexArrayID;
@@ -107,23 +195,65 @@ private:
 	FirstPersonCamera camera;
 	MS modelStack, viewStack, projectionStack;
 	Light light[5];
+	Notification notifs = Notification::N_OFF;
 
 	//functions to render things
 	void RenderMesh(Mesh* mesh, bool enableLight);
 	void RenderSkybox();
+	void RenderPhoneUI();
+	void RenderComputerUI();
+	void RenderNotifications();
 	void RenderText(Mesh* mesh, std::string text, Color color);
-	void RenderOfficeSpace();
 
 	//use screen points
-	void RenderTextOnScreen(Mesh* mesh, std::string text, Color color, float size, float x, float y);
-	void RenderImageOnScreen(Mesh* mesh, Color color, float sizex = 1, float sizey = 1, float x = 0, float y = 0);
+	void RenderTextOnScreen(Mesh* mesh, std::string text, Color color, float size = 1, float x = 0, float y = 0, float xRotation = 0, float zRotation = 0);
+	void RenderImageOnScreen(Mesh* mesh, Color color, float sizex = 1, float sizey = 1, float x = 0, float y = 0, float xRotation = 0, float zRotation = 0);
 	std::vector<float> getNumberValues(std::string filename);
 	std::vector<float> fontData;
 
 	bool enableLight;
+	bool keyToggle;
+	bool mouseToggle;
+
+	//in-game stuff
+	Clock* clock;
+	player* Player;
+	Phone* phone;
+	std::string clockTime = "";
+	int money = int(DEFAULTMONEY), socialMeter = int(DEFAULTSOCIALMETER);
+	double xpos, ypos;
+	float NotificationTimer = 2;
+	bool hasReceivedTasks = false;
+	int taskNumber = 1;
+	Notification notif;
+
+	//phone ??
+	PhoneScreen phoneState;
+	int taskCount, chatCount;
+	bool isPhoneOpen = false;
+	bool hoverAnimation[9] = { false, false, false, false, false, false, false, false, false };
+	//just a general page counter
+	int pageCounter;
+
+	//work computer 
+	float angleBetweenComputer = 0;
+	bool isUsingComputer;
+	bool comHoverAnimation[6] = { false, false, false, false, false, false };
+	//TODO: if there's time make the emails into a struct thanks
+	std::vector<bool> isScamEmails;
+	std::vector<std::pair<std::vector<std::string>, std::string>> emails;
+	void initialiseEmails(std::string textFile);
+	int numUnreadEmails;
+	computerScreen computerState;
+	float workTimer = 0;
+	int workTasksLeft = 10;
+	bool hasClockedIn = false;
+
+	//shopping game
+	int moneySpent = 0;
+	std::vector<std::pair<std::vector<products>, std::string>> shopList;
+	void initialiseShop(std::string textFile);
 };
-
-
 
 
 #endif
